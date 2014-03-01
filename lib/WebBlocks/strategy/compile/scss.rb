d4ed117354fda2d6308797e28537b6fbcb812d2a
Eric Bollens <ebollens@ucla.edu>
2014-03-01 13:50:26 -0800

    Refactor Scss compile strategy

--- lib/WebBlocks/strategy/compile/scss.rb
+++ lib/WebBlocks/strategy/compile/scss.rb
@@ -8,11 +8,17 @@ module WebBlocks
       class Scss
 
         attr_reader :log
+        attr_reader :io
+        attr_reader :workspace_path
+        attr_reader :cache_path
 
         def initialize task, log
 
           @task = task
           @log = log.scope 'Compile'
+
+          @workspace_path = @task.base_path + '.blocks/workspace'
+          @cache_path = @task.base_path + '.blocks/cache/sass'
 
         end
 
@@ -20,14 +26,21 @@ module WebBlocks
 
           log.info { "Starting" }
 
-          workspace_path = @task.base_path + '.blocks/workspace'
-          cache_path = @task.base_path + '.blocks/cache/sass'
-          options = {}
-
           output = StringIO.new
 
-          Compass::Logger.send(:define_method, :color) { |c| "" }
-          Compass::Logger.send(:define_method, :log) { |msg| output.puts msg }
+          with_compass_io output do
+            configure_compass!
+            compile_files!
+          end
+
+          output.rewind
+          output.each_line { |line| log.debug("Compass"){ line.to_s.gsub(/\n/,'') } }
+
+          log.info { "Finished" }
+
+        end
+
+        def configure_compass!
 
           Compass.add_configuration({
               :project_path => workspace_path,
@@ -38,16 +51,39 @@ module WebBlocks
             'blocks'
           )
 
-          from = (workspace_path + 'scss/blocks.scss')
-          to = (workspace_path + 'css/blocks.css')
+        end
 
-          FileUtils.mkdir_p to.parent
-          Compass.compiler.compile from.to_s, to.to_s
+        def compile_files!
 
-          output.rewind
-          output.each_line { |line| log.debug("Compass"){ line.to_s.gsub(/\n/,'') } }
+          compile_file workspace_path + 'scss/blocks.scss', workspace_path + 'css/blocks.css'
 
-          log.info { "Finished" }
+        end
+
+        def compile_file src, dst
+
+          FileUtils.mkdir_p dst.parent
+          Compass.compiler.compile src.to_s, dst.to_s
+
+        end
+
+        def with_compass_io io
+
+          alias_mapping = {
+            :color => Proc.new { |c| "" },
+            :log => Proc.new { |msg| io.puts msg }
+          }
+
+          alias_mapping.each do |method, proc|
+            Compass::Logger.send :alias_method, "___#{method}", method
+            Compass::Logger.send :define_method, method, &proc
+          end
+
+          yield
+
+          alias_mapping.keys.each do |method|
+            Compass::Logger.send :alias_method, method, "___#{method}"
+            Compass::Logger.send :remove_method, "___#{method}"
+          end
 
         end
 
